b260cfbe6e14c733455dd38b414dbdcefa0d5660
nirali8 <nirali.skillqode@gmail.com>
2024-07-11 18:40:05 +0530

    MAth Properties

---
+++ Lecture10/math.js
@@ -0,0 +1,43 @@
+/*MAths constants */
+
+console.log("eular formula = " + Math.E);
+console.log("Pi = " + Math.PI);
+console.log("LN10 = " + Math.LN10);
+console.log("LN2  = " + Math.LN2);
+console.log("Log10E = " + Math.LOG10E);
+console.log("Log2E = " + Math.LOG2E);
+console.log("sqrt2 = " + Math.SQRT2);
+console.log("sqrt1/2 = " + Math.SQRT1_2);
+
+/*MAths methods */
+console.log(Math.ceil(12.89));
+console.log(Math.ceil(-12.89));
+console.log(Math.floor(12.89));
+console.log(Math.floor(-12.89));
+console.log(Math.round(12.89));
+console.log(Math.round(-12.89));
+console.log(Math.trunc(12.89));
+console.log(Math.trunc(-12.89));
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(106));
+console.log(Math.pow(2, 3));
+console.log(Math.sign(12));
+console.log(Math.sign(-12));
+console.log(Math.sign(0));
+console.log(Math.abs(25));
+console.log(Math.abs(25.44));
+console.log(Math.abs(-25));
+console.log(Math.abs(-25.55));
+console.log(Math.max(25, 1, 33, 2, 10, 11, 9, 55, -12));
+console.log(Math.min(25, 1, 33, 2, 10, 11, 9, 55, -12));
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.sin(90));
+console.log(Math.cos(90));
+console.log(Math.sin((90 * Math.PI) / 180));
+console.log(Math.cos((90 * Math.PI) / 180));
+console.log(Math.log10(5000));
+console.log(Math.log10(1000));
+console.log(Math.log2(5000));
+console.log(Math.log(100));
